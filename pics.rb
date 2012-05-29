b0c9306261adde4defebd08f0f430846976d5dc7
Kirill Maksimov <netoneko@gmail.com>
2012-05-30 02:21:07 +0600

    more stuff

--- pics.rb
+++ pics.rb
@@ -4,41 +4,37 @@ require "bundler/setup"
 require 'rvg/rvg'
 require 'coderay'
 
-tokens = CodeRay.scan File.read(__FILE__), :ruby
+lines = File.readlines(__FILE__)
+tokens = CodeRay.scan lines.join, :ruby
 puts tokens.inspect
 puts tokens.statistic
-
-
-Magick::RVG::dpi = 90
+puts lines.count
 
 fill = {
   content: "darkgrey",
   delimiter: 'darkgrey',
   operator: 'green',
-  ident: 'black',
+  ident: 'orange',
   symbol: 'magenta',
   instance_variable: 'pink',
   keyword: 'red',
   constant: 'pink'
 }
 
-rvg = Magick::RVG.new(30.cm, 30.cm).viewbox(0,0, 1200, 1200) do |canvas|
+rvg = Magick::RVG.new(1000, lines.size * 25) do |canvas|
   canvas.background_fill = 'white'
   canvas.desc = "Example tspan02 - using tspan's dx and dy attributes for incremental positioning adjustments"
   canvas.g.styles(:font_family=>'Lucida Console', :font_size=> 15) do |_g|
     top_index = 0
 
-    tokens.each_with_index do |token, index|
-      puts token.inspect
-
+    tokens.each do |token|
       if token.kind_of? String
         @value = token
       elsif @value
         if (token == :space && @value == "\n") || !defined?(@text)
           @text = _g.text(10, (top_index += 1) * 25, @value.to_s).styles(:fill => fill[token] || 'black')
-          puts "NEWLINE"
         else
-          @text.tspan(@value.to_s).d(@value.size * 0.5, 0).styles(:fill => fill[token] || 'black')
+          @text.tspan(@value.to_s).d(0, 0).styles(:fill => fill[token] || 'black')
         end
 
         @value = nil
